d27ae5b03c70794805b42a1b4f9a7aeaa2c7511e
v_xhangxie <v_xhangxie@tencent.com>
2021-12-21 18:49:15 +0800

    2021年12月21日 18:48:21

--- app/src/main/java/net/basicmodel/MainActivity.kt
+++ app/src/main/java/net/basicmodel/MainActivity.kt
@@ -2,13 +2,20 @@ package net.basicmodel
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.yanzhenjie.nohttp.NoHttp
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.layout_float.*
 import net.utils.MyFragmentManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initBottomBar()
+        initMapType()
+    }
+
+    private fun initBottomBar(){
         MyFragmentManager.get().showFragment(0, supportFragmentManager)
         val navigationController = tab.material()
             .addItem(R.mipmap.map, "Map")
@@ -18,6 +25,24 @@ class MainActivity : AppCompatActivity() {
             .build()
         navigationController.addSimpleTabItemSelectedListener { index, old ->
             MyFragmentManager.get().showFragment(index, supportFragmentManager)
+        }
+    }
+
+    private fun initMapType(){
+        search.setOnClickListener {
+
+        }
+        normal.setOnClickListener {
+
+        }
+        hybird.setOnClickListener {
+
+        }
+        sat.setOnClickListener {
+
+        }
+        terrain.setOnClickListener {
+
         }
     }
 }
--- app/src/main/java/net/fragment/InterFragment.kt
+++ app/src/main/java/net/fragment/InterFragment.kt
@@ -1,9 +1,17 @@
 package net.fragment
 
+import android.os.Bundle
+import android.view.View
+import com.yanzhenjie.nohttp.RequestMethod
+import com.yanzhenjie.nohttp.rest.StringRequest
 import net.basicmodel.R
 
 class InterFragment : BaseFragment() {
     override fun getLayout(): Int {
         return R.layout.fragment_inter
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
 }
